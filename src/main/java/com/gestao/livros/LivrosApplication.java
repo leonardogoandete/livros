41a5119906629c1965fcd5f651299bc08aaa131d
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2021-12-08 12:00:49 -0300

    Criptografado senha do banco aplication.properties;

--- src/main/java/com/gestao/livros/LivrosApplication.java
+++ src/main/java/com/gestao/livros/LivrosApplication.java
@@ -1,9 +1,11 @@
 package com.gestao.livros;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class LivrosApplication {
 
 	public static void main(String[] args) {
